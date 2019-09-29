0db9f5cd735b00367406cfb8554a0ae00e0c29ff
Rafik BELAS <belasrafik@gmail.com>
2019-09-29 14:26:03 +0100

    Corrected application model to detect controllers.

--- src/application.module.ts
+++ src/application.module.ts
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import glob from "glob";
 
 const controllers = glob
-  .sync("*.module/*-controllers.ts", { cwd: __dirname, absolute: true }) // go through all the modules containing controllers
+  .sync("*.module/*-controller.ts", { cwd: __dirname, absolute: true }) // go through all the modules containing controllers
   .map(require) // require every one of them
   .map(imported => imported.default); // and return each one's default export (which is expected to be a NestJS controller class)
 
