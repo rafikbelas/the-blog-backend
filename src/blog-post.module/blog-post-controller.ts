7d912f316d0bf0d843863a75d500ca1ff86dd76f
Rafik BELAS <belasrafik@gmail.com>
2019-09-29 17:25:06 +0100

    Added create a blog post.

--- src/blog-post.module/blog-post-controller.ts
+++ src/blog-post.module/blog-post-controller.ts
@@ -1,5 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  NotFoundException,
+  Param,
+  Req,
+  UseGuards
+} from "@nestjs/common";
 import { ApiModelProperty, ApiResponse } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+import { IsAuthenticatedGuard } from "../auth.module/is-authenticated-guard";
+import { getUserById } from "../auth.module/okta-client";
+import { User } from "../user.module/user-controller";
 
 export class BlogPost {
   @ApiModelProperty() // Used to generate Swagger documentation that `BlogPost` model contains id of type number.
@@ -15,26 +28,17 @@ export class BlogPost {
   content: string;
 }
 
-export const blogPosts = [
-  {
-    id: 1,
-    authorId: "xxx",
-    title: "Build a NodeJS app with TypeScript",
-    content: "Whats wrong with JavaScript?"
-  },
-  {
-    id: 2,
-    authorId: "yyy",
-    title: "D'ont build a NodeJS app with TypeScript",
-    content: "Whats wrong with TypeScript?"
-  },
-  {
-    id: 3,
-    authorId: "zzz",
-    title: "Build a Java app with Spring Boot",
-    content: "Whats wrong with J2EE?"
-  }
-];
+export class BlogPostDto {
+  @ApiModelProperty()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiModelProperty()
+  @IsNotEmpty()
+  content: string;
+}
+
+export const blogPosts = new Array<BlogPost>();
 
 @Controller("blog-posts")
 export default class BlogPostController {
@@ -42,5 +46,29 @@ export default class BlogPostController {
   @ApiResponse({ type: BlogPost, status: 200, isArray: true }) // For Swagger documentation: API returns an array of BlogPost models.
   findAll(): Array<BlogPost> {
     return blogPosts;
+  }
+
+  @Post()
+  @ApiResponse({ type: BlogPost, status: 201 })
+  @UseGuards(IsAuthenticatedGuard)
+  create(@Body() blogPostDto: BlogPostDto, @Req() req): BlogPost {
+    const { content, title } = blogPostDto;
+    const id = blogPosts.length + 1;
+    const { userId } = req["auth"];
+
+    const newBlogPost: BlogPost = { id, title, content, authorId: userId };
+    blogPosts.push(newBlogPost);
+
+    return newBlogPost;
+  }
+
+  @Get(":id/author")
+  async findAuthor(@Param("id") blogPostId: string): Promise<User> {
+    const blog = blogPosts.find(post => post.id.toString() === blogPostId);
+    if (!blog) {
+      throw new NotFoundException("No such blog post.");
+    }
+
+    return await getUserById(blog.authorId);
   }
 }
